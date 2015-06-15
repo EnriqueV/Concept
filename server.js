eb1dcf234ba4de09f57e969c49c0935009a1c87f
Eduardo Valencia <kalienrique40@gmail.com>
2015-06-14 20:59:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+http.listen(process.env.PORT || 3000,function(){
+console.log('The server listen to %s',PORT);
+});
